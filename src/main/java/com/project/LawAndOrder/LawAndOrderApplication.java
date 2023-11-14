a69b1dadae2ae5df06c09d09f04eaea90ccbfd93
Bartemnius <wojtekdluski@wp.pl>
2023-11-14 14:54:13 +0100

    Adding all existing files to git repo

---
+++ src/main/java/com/project/LawAndOrder/LawAndOrderApplication.java
@@ -0,0 +1,13 @@
+package com.project.LawAndOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawAndOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawAndOrderApplication.class, args);
+	}
+
+}
